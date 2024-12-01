93d4bed787fdbecd777a20db7ac449710c6930f7
mikomaco <84585119+m1k0103@users.noreply.github.com>
2024-12-01 16:25:16 +0000

    tried writing a whole function for this, glad i checked iftheres an easier solution lol

---
+++ find_between.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string str = "hello there! my name is mikolaj and i am a terrible programmer! lol";
+    unsigned first = str.find("there!");
+    unsigned last = str.find_last_of("!");
+    string strBetween = str.substr(first, last-first);
+    cout << strBetween;
+}
